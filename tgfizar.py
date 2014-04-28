a242d8f8f7627cc65d9b89aea2fa9797942b86d0
Lautaro Petaccio <lausuper@gmail.com>
2014-04-28 14:54:42 -0300

    Tgfizar contempla más de un root y una arista por sender y reciever

--- tgfizar.py
+++ tgfizar.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 import sys
 
-if len(sys.argv) < 2:
-	sys.exit("Usage: python2 tgfizar.py file_path ip_root")
+if len(sys.argv) < 1:
+	sys.exit("Usage: python2 tgfizar.py file_path")
 
 
 ips_en_red = list()
+#Lista de tuplas (Ip-Src, Ip-Dst)
+messages = list()
 f = open(sys.argv[1], 'r')
+#Conisgo todas las IP's
 for line in f:
 	#Sacar ips y ponerlas en list
 	datos = line.split(", ")
+
+	if datos[0] == "ARP-Operacion":
+		continue
 
 	#Ip-Src esta en la posicion 3
 	if not (datos[3] in ips_en_red):
@@ -19,12 +25,18 @@ for line in f:
 	if not (datos[4] in ips_en_red):
 		ips_en_red.append(datos[4])
 
+	#Guardo mensaje
+	messages.append((ips_en_red.index(datos[3]),ips_en_red.index(datos[4])))
+
 for i in range(len(ips_en_red)):
 	print str(i+1) + " " + ips_en_red[i]
 
 print "#"
 
-for i in range(len(ips_en_red)):
-	print str(datos.index(sys.argv[2])+1) + " " + str(i+1)
+writen_messages = list()
+for msg in messages:
+	if not (msg in writen_messages):
+		print str(msg[0]+1) + " " + str(msg[1]+1)
+		writen_messages.append(msg)
 
 f.close()
